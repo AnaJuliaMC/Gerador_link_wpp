b4ac3bd301aeb56d5d7176ee47016bb80f010955
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-05-01 15:16:30 -0300

    começando o js

---
+++ script.js
@@ -0,0 +1,11 @@
+const BntLink = document.querySelector("BntLink")
+const BntMens = document.querySelector("BntMensagem")
+const link = document.querySelector("link")
+const mensagem = document.querySelector("mensagem")
+
+BntLink.addEventListener("click", handleBtnLink)
+
+function handleBtnLink(evento){
+    
+
+}
