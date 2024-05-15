cf63f877670512e732e0f385bcc5af78afab1675
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-05-15 11:45:36 -0300

    copiar link e selecionar

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const BntLink = document.getElementById('bntLink');
 const BntSend = document.getElementById('bntSend');
-let link = document.getElementById("link");
-const mensagem = document.querySelector("mensagem");
+const link = document.getElementById("link");
+const mensagem = document.getElementById("mensagem");
 const numero = document.getElementById("numero")
 
 BntLink.addEventListener("click", handleBtnLink)
 BntSend.addEventListener("click", handleBtnSend)
+link.addEventListener("click", handleLink)
 
 function handleBtnLink(){
     var TelLink = 'https://wa.me/55'+numero.value
     link.innerText = TelLink
-    console.log()
-
-   
-
+    link.style.border = "1px solid black"
+    link.style.borderRadius = "5px"
+    mensagem.textContent = "Clique no link para copiar"
 }
 
 function handleBtnSend(){
@@ -22,4 +22,8 @@ function handleBtnSend(){
     janela.focus();
   }
 
-
+function handleLink(){
+    navigator.clipboard.writeText(link.textContent)
+    .then(() => mensagem.textContent = "Texto copiado com sucesso!")
+    .catch(err => console.error("Falha ao copiar o texto:", err));
+}
