f711a99d93dec26033bb0f3d3332d423e25dec7d
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-05-17 11:01:15 -0300

    validacao e mascara
    fazendo a validacao do numero, aplicando mascara, e etc.

--- script.js
+++ script.js
@@ -7,9 +7,17 @@ const numero = document.getElementById("numero")
 BntLink.addEventListener("click", handleBtnLink)
 BntSend.addEventListener("click", handleBtnSend)
 link.addEventListener("click", handleLink)
+numero.addEventListener("click", handleInput)
+numero.addEventListener('input', ()=> {
+    numero.value = mascara(numero.value);
+    validacao()
+
+})
+
 
 function handleBtnLink(){
-    var TelLink = 'https://wa.me/55'+numero.value
+    let numeroTel = numero.value.replace(/\D/g, '')
+    var TelLink = 'https://wa.me/55'+numeroTel
     link.innerText = TelLink
     link.style.border = "1px solid black"
     link.style.borderRadius = "5px"
@@ -17,7 +25,8 @@ function handleBtnLink(){
 }
 
 function handleBtnSend(){
-    var TelLink = 'https://wa.me/55'+numero.value
+    let numeroTel = numero.value.replace(/\D/g, '')
+    var TelLink = 'https://wa.me/55'+numeroTel
     var janela = window.open(TelLink, '_blank');
     janela.focus();
   }
@@ -27,3 +36,35 @@ function handleLink(){
     .then(() => mensagem.textContent = "Texto copiado com sucesso!")
     .catch(err => console.error("Falha ao copiar o texto:", err));
 }
+
+function validacao(){
+    const numValido = numero.value.length === 15
+    if (numValido){
+        BntLink.disabled = false
+        BntSend.disabled = false
+        numero.style.color = "green"
+    }else{
+        BntLink.disabled = true
+        BntSend.disabled = true
+        numero.style.color = "black"
+    }
+}
+
+function mascara(){
+    let numeroTel = numero.value.replace(/\D/g, '')
+    numeroTel = numeroTel.replace(/\D/g, '');
+    numeroTel = numeroTel.slice(0,11);
+    numeroTel = numeroTel.replace(/(\d{2})(\d{4,5})(\d{4})/, '($1) $2-$3')
+    return numeroTel;
+
+}
+
+function handleInput(){
+    mensagem.textContent = ""
+    link.style.border = "0"
+    link.innerText = ""
+
+}
+
+BntLink.disabled = true
+BntSend.disabled = true
